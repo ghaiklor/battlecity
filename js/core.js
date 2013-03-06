223fd2cb8bd02b452280a6bd84ce2535e3dab293
ghaiklor <ghaiklor@ghaiklor-PC.(none)>
2013-03-06 16:13:37 +0200

    I'm rewrite all code in prototype style. All objects have own prototype. Realized centring map in Scene. Added function to load mask of map for drawing saved maps. Added functions to check collision tank with edges of map. TODO: 1. First I need to create map editor to edit and create own maps. 2. Add check collision on blocks (steel and brick) 3. Add check for current position tank for change speed (on forest or water)

--- js/core.js
+++ js/core.js
@@ -1,20 +1,26 @@
+/////////////////////////////////TODO://////////////////////////////////////////
+///////1. Fix the bug on check collision tank with map (right and down)/////////
+///////2. Realize map editor////////////////////////////////////////////////////
+///////3. Realize check tank on collision with blocks///////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 //////////////////Class: Scene//////////////////////////////////////////////////
 /////////////This is a class of scene, where will drawing game objects//////////
 //Attributes: canvas, context, width, height////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 function Scene(id) {
-    this.recalcSize = function() {
+    this.canvas = document.getElementById(id);
+    this.context = this.canvas.getContext('2d');
+    return this;
+}
+
+Scene.prototype = {
+    recalcSize: function() {
         this.width = window.innerWidth;
         this.height = window.innerHeight;
         this.canvas.width = this.width;
         this.canvas.height = this.height;
-    };
-
-    this.canvas = document.getElementById(id);
-    this.context = this.canvas.getContext('2d');
-    this.recalcSize();
-}
+    }
+};
 ////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////Class: Game//////////////////////////////////////////
 /////////////It's main class of game. Here is redrawing and timer of game///////
@@ -22,63 +28,68 @@ function Scene(id) {
 ////////////////////////////////////////////////////////////////////////////////
 function Game() {
     this.gameTimer = null;
+    return this;
+}
 
-    this.globalUpdateValues = function() {
+Game.prototype = {
+    globalUpdateValues: function() {
         if (Core.Variables.Keyboard.keyPressed) {
             Core.Variables.PlayerTank.moveTank();
+            //Core.Variables.Console.writeDebug('Key is Pressed in globalUpdateValues');
         }
-    };
-
-    this.drawMap = function(scene, map) {
-        for (var w_index = 0; w_index < Core.Variables.Map.countTilesWidth; w_index++) {
-            for (var h_index = 0; h_index < Core.Variables.Map.countTilesHeight; h_index++) {
-                /////////////////////////////////////////////////////////////////////////////
-                //////////////////////////////////TODO://////////////////////////////////////
-                /////////////////////////////////////////////////////////////////////////////
-                var random = Math.floor(Math.random() * 4);
-                switch (random) {
+    },
+    drawMap: function(scene, map) {
+        for (var h_index = 0; h_index < map.countTilesHeight; h_index++) {
+            for (var w_index = 0; w_index < map.countTilesWidth; w_index++) {
+                switch (map.mask[h_index][w_index]) {
                     case 0:
-                        scene.context.drawImage(map.environment.brick, Core.Config.tileEnvironmentWidth * w_index, Core.Config.tileEnvironmentHeight * h_index, Core.Config.tileEnvironmentWidth, Core.Config.tileEnvironmentHeight);
+                        scene.context.drawImage(map.environment.brick, Core.Config.tileEnvironmentWidth * w_index + Core.Variables.Map.offsetXMap, Core.Config.tileEnvironmentHeight * h_index + Core.Variables.Map.offsetYMap, Core.Config.tileEnvironmentWidth, Core.Config.tileEnvironmentHeight);
                         break;
                     case 1:
-                        scene.context.drawImage(map.environment.forest, Core.Config.tileEnvironmentWidth * w_index, Core.Config.tileEnvironmentHeight * h_index, Core.Config.tileEnvironmentWidth, Core.Config.tileEnvironmentHeight);
+                        scene.context.drawImage(map.environment.forest, Core.Config.tileEnvironmentWidth * w_index + Core.Variables.Map.offsetXMap, Core.Config.tileEnvironmentHeight * h_index + Core.Variables.Map.offsetYMap, Core.Config.tileEnvironmentWidth, Core.Config.tileEnvironmentHeight);
                         break;
                     case 2:
-                        scene.context.drawImage(map.environment.steel, Core.Config.tileEnvironmentWidth * w_index, Core.Config.tileEnvironmentHeight * h_index, Core.Config.tileEnvironmentWidth, Core.Config.tileEnvironmentHeight);
+                        scene.context.drawImage(map.environment.steel, Core.Config.tileEnvironmentWidth * w_index + Core.Variables.Map.offsetXMap, Core.Config.tileEnvironmentHeight * h_index + Core.Variables.Map.offsetYMap, Core.Config.tileEnvironmentWidth, Core.Config.tileEnvironmentHeight);
                         break;
                     case 3:
-                        scene.context.drawImage(map.environment.water, Core.Config.tileEnvironmentWidth * w_index, Core.Config.tileEnvironmentHeight * h_index, Core.Config.tileEnvironmentWidth, Core.Config.tileEnvironmentHeight);
+                        scene.context.drawImage(map.environment.water, Core.Config.tileEnvironmentWidth * w_index + Core.Variables.Map.offsetXMap, Core.Config.tileEnvironmentHeight * h_index + Core.Variables.Map.offsetYMap, Core.Config.tileEnvironmentWidth, Core.Config.tileEnvironmentHeight);
                         break;
                 }
-                /////////////////////////////////////////////////////////////////////////////TODO: OpenThisFuncAfterDeleteUppersFuckingShit
-                //scene.context.drawImage(map.environment.forest, Core.Config.tileEnvironmentWidth * w_index, Core.Config.tileEnvironmentHeight * h_index, Core.Config.tileEnvironmentWidth, Core.Config.tileEnvironmentHeight);
             }
         }
-    };
-    this.drawTank = function(scene, tank) {
+    },
+    drawTank: function(scene, tank) {
         scene.context.beginPath();
         scene.context.drawImage(tank.image, tank.direction * tank.width, 0, tank.width, tank.height, tank.x, tank.y, tank.width, tank.height);
         scene.context.closePath();
-    };
-    this.clearScene = function(scene) {
+    },
+    clearScene: function(scene) {
         scene.context.clearRect(0, 0, scene.width, scene.height);
-    };
-    this.drawScene = function() {
+        scene.context.beginPath();
+        scene.context.fillStyle = 'rgba(70, 70, 70, 0.5)';
+        scene.context.rect(0, 0, scene.width, scene.height);
+        scene.context.fill();
+        scene.context.closePath();
+    },
+    drawScene: function() {
         this.globalUpdateValues();
         this.clearScene(Core.Variables.MainScene);
         this.drawMap(Core.Variables.MainScene, Core.Variables.Map);
         this.drawTank(Core.Variables.MainScene, Core.Variables.PlayerTank);
-    };
-}
+    }
+};
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////Class: Keyboard/////////////////////////////
 ///////////////Here I'm check input and parse it////////////////////////////////
 ///////////Attributes: keyPressed and all's other it's functions////////////////
 ////////////////////////////////////////////////////////////////////////////////
 function Keyboard() {
-    var self = this;
+    this.keyPressed = false;
+    return this;
+}
 
-    this.convertCodeToDirection = function(code) {
+Keyboard.prototype = {
+    convertCodeToDirection: function(code) {
         switch (code) {
             case 37:
                 return 'left';
@@ -91,9 +102,8 @@ function Keyboard() {
             default:
                 return false;
         }
-    };
-    this.keyPressed = false;
-    this.keyDown = function(e) {
+    },
+    keyDown: function(e) {
         var direction = this.convertCodeToDirection(e.keyCode);
         if (direction) {
             Core.Variables.PlayerTank.setDirection(direction);
@@ -101,40 +111,44 @@ function Keyboard() {
             //Core.Variables.Console.writeDebug('Key is press: ' + this.keyPressed);
             //Core.Variables.Console.writeDebug('Current direction: ' + direction);
         }
-    };
-    this.keyUp = function(e) {
+    },
+    keyUp: function(e) {
         var direction = this.convertCodeToDirection(e.keyCode);
         if (direction) {
             this.keyPressed = false;
             //Core.Variables.Console.writeDebug('Key is press: ' + this.keyPressed);
         }
-    };
-    this.hookEvent = function() {
+    },
+    hookEvent: function() {
+        var self = this;
         document.onkeydown = function(e) {
             self.keyDown(e);
         };
         document.onkeyup = function(e) {
             self.keyUp(e);
         };
-    };
-    this.hookEvent();
-}
+    }
+};
 ////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////Class: Game Timer////////////////////////////////
 //////////////////It's a main timer of game. We can start or stop game//////////
 ////////////////////////////////////////////////////////////////////////////////
 function GameTimer() {
-    this.startGame = function(game) {
+    return this;
+}
+
+GameTimer.prototype = {
+    startGame: function(game) {
         game.gameTimer = setInterval(function() {
             game.drawScene();
         }, Core.Config.UpdateTimerInterval);
         Core.Variables.Console.writeInfo('Game is start');
-    };
-    this.stopGame = function(game) {
+    },
+    stopGame: function(game) {
         clearInterval(game.gameTimer);
         Core.Variables.Console.writeInfo('Game is stoped');
-    };
-}
+    }
+};
 ////////////////////////////////////////////////////////////////////////////////
 /////////////////////////Class: Tank////////////////////////////////////////////
 ////////Here a class of Tank. All functions inside works with tank./////////////
@@ -142,23 +156,66 @@ function GameTimer() {
 ///strDirection ('up', 'right', 'left', 'down'), direction (integer), speed/////
 ////////////////////////////////////////////////////////////////////////////////
 function Tank(imageSrc, x, y, width, height, direction, speed) {
-    this.moveTank = function() {
-        switch (this.strDirection) {
-            case 'up':
-                this.y = this.y - this.speed;
-                break;
-            case 'down':
-                this.y = this.y + this.speed;
-                break;
-            case 'left':
-                this.x = this.x - this.speed;
-                break;
-            case 'right':
-                this.x = this.x + this.speed;
-                break;
+    var image = new Image();
+    image.src = imageSrc;
+    this.image = image;
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.strDirection = direction;
+    this.setDirection(direction);
+    this.speed = speed;
+    return this;
+}
+
+Tank.prototype = {
+    checkInsideMapUp: function(map) {
+        if (this.y - this.speed >= map.offsetYMap) {
+            Core.Variables.Console.writeDebug('Tank inside map up');
+            return true;
+        } else {
+            return false;
         }
-    };
-    this.convertDirectionToInteger = function(direction) {
+    },
+    checkInsideMapRight: function(map) {
+        if (this.x + this.speed <= map.offsetXMap + map.mapWidth - this.width + this.speed - 6) { //TODO: fix this bug
+            Core.Variables.Console.writeDebug('Tank inside map right');
+            return true;
+        } else {
+            return false;
+        }
+    },
+    checkInsideMapDown: function(map) {
+        if (this.y + this.speed < map.offsetYMap + map.mapHeight - this.height) { //TODO: fix this bug
+            Core.Variables.Console.writeDebug('Tank inside map down');
+            return true;
+        } else {
+            return false;
+        }
+    },
+    checkInsideMapLeft: function(map) {
+        if (this.x >= map.offsetXMap + this.speed) {
+            Core.Variables.Console.writeDebug('Tank inside map left');
+            return true;
+        } else {
+            return false;
+        }
+    },
+    checkCollisionBlocks: function() { //TODO: realize check for driving and collise with blocks
+        var tankInsideMap = this.checkInsideMap(Core.Variables.Map);
+        if (!tankInsideMap) {
+            return false;
+        } else {
+            return true;
+        }
+        //Core.Variables.Console.writeDebug('Collision is checked');
+    },
+    setDirection: function(direction) {
+        this.strDirection = direction;
+        this.direction = this.convertDirectionToInteger(direction);
+    },
+    convertDirectionToInteger: function(direction) {
         switch (direction) {
             case 'up':
                 return 2;
@@ -173,68 +230,103 @@ function Tank(imageSrc, x, y, width, height, direction, speed) {
                 return 0;
                 break;
         }
-    };
-
-    this.setDirection = function(direction) {
-        this.strDirection = direction;
-        this.direction = this.convertDirectionToInteger(direction);
-    };
-
-    var image = new Image();
-    image.src = imageSrc;
-    this.image = image;
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
-    this.strDirection = direction;
-    this.setDirection(direction);
-    this.speed = speed;
-}
+    },
+    moveTank: function() {
+        Core.Variables.Console.writeDebug('Tank X = ' + this.x + ', Tank Y = ' + this.y);
+        switch (this.strDirection) {
+            case 'up':
+                if (this.checkInsideMapUp(Core.Variables.Map)) {
+                    this.y = this.y - this.speed;
+                }
+                break;
+            case 'down':
+                if (this.checkInsideMapDown(Core.Variables.Map)) {
+                    this.y = this.y + this.speed;
+                }
+                break;
+            case 'left':
+                if (this.checkInsideMapLeft(Core.Variables.Map)) {
+                    this.x = this.x - this.speed;
+                }
+                break;
+            case 'right':
+                if (this.checkInsideMapRight(Core.Variables.Map)) {
+                    this.x = this.x + this.speed;
+                }
+                break;
+        }
+    }
+};
 ////////////////////////////////////////////////////////////////////////////////
 //////////////////////////Class: Map////////////////////////////////////////////
 ///////////////In this class images, loader, array, etc/////////////////////////
 ////////Attributes: countTilesWidth, countTilesHeight///////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 function Map() {
-    this.calculateTilesCount = function() {
-        this.countTilesWidth = Core.Variables.MainScene.width / 24;
-        this.countTilesHeight = Core.Variables.MainScene.height / 24;
-    };
-    this.loadImage = function(src) {
+    this.countTilesHeight = 8;
+    this.countTilesWidth = 8;
+    this.offsetXMap = 100;
+    this.offsetYMap = 100;
+    return this;
+}
+
+Map.prototype = {
+    calculateOffsetMap: function() {
+        this.mapWidth = this.countTilesWidth * Core.Config.tileEnvironmentWidth;
+        this.mapHeight = this.countTilesHeight * Core.Config.tileEnvironmentHeight;
+        var centerMapX = Core.Variables.MainScene.width / 2;
+        var centerMapY = Core.Variables.MainScene.height / 2;
+        this.offsetXMap = centerMapX - this.mapWidth / 2;
+        this.offsetYMap = centerMapY - this.mapHeight / 2;
+    },
+    loadImage: function(src) {
         var image = new Image();
         image.src = src;
         return image;
-    };
-    this.environment = {
-        brick: this.loadImage(Core.Config.tileBrickSrc),
-        forest: this.loadImage(Core.Config.tileForestSrc),
-        steel: this.loadImage(Core.Config.tileSteelSrc),
-        water: this.loadImage(Core.Config.tileWaterSrc)
-    };
-    this.calculateTilesCount();
-}
+    },
+    loadResources: function() {
+        this.environment.brick = this.loadImage(Core.Config.tileBrickSrc);
+        this.environment.forest = this.loadImage(Core.Config.tileForestSrc);
+        this.environment.steel = this.loadImage(Core.Config.tileSteelSrc);
+        this.environment.water = this.loadImage(Core.Config.tileWaterSrc);
+    },
+    environment: {
+        brick: null,
+        forest: null,
+        steel: null,
+        water: null
+    },
+    loadMask: function(src) { //TODO: create Map Editor and realize saving
+        this.mask = [[0, 1, 0, 2, 2, 3, 1, 0], [0, 3, 0, 2, 3, 2, 3, 1], [0, 2, 2, 1, 2, 1, 2, 3], [0, 3, 3, 1, 2, 3, 1, 3], [0, 2, 3, 2, 1, 1, 2, 0]];
+        this.countTilesHeight = 5;
+        this.countTilesWidth = 8;
+    }
+};
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////Class: Console//////////////////////////////////
 /////////////////////////////Just for Debugging/////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 function Console() {
-    this.checkDebugMode = function() {
+    return this;
+}
+
+Console.prototype = {
+    checkDebugMode: function() {
         if (Core.Config.debugMode) {
             return true;
         } else {
             return false;
         }
-    };
-    this.writeInfo = function(text) {
+    },
+    writeInfo: function(text) {
         console.info(text);
-    };
-    this.writeDebug = function(text) {
+    },
+    writeDebug: function(text) {
         if (this.checkDebugMode()) {
             console.debug(text);
         }
-    };
-}
+    }
+};
 ////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////CORE OF MY GAME////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
@@ -243,8 +335,8 @@ var Core = {
         debugMode: true, //Debug mode enabled
         tileTankWidth: 48, //Width of one sprite (tank)
         tileTankHeight: 48, //Height of one sprite (tank)
-        tileEnvironmentWidth: 24, //Width of one tile (environment)
-        tileEnvironmentHeight: 24, //Height of one tile (environment)
+        tileEnvironmentWidth: 50, //Width of one tile (environment)
+        tileEnvironmentHeight: 50, //Height of one tile (environment)
         tilesCount: 8, //Count of tiles in one line (in this case size of field 8 * 8)
         tileTankSrc: './images/tank.png', //Source to Tank Image
         tankSpeed: 10, //Default speed of tank
@@ -270,9 +362,14 @@ var Core = {
     InitializeGame: function() {
         Core.Variables.Console = new Console(); //DONE
         Core.Variables.Keyboard = new Keyboard(); //DONE
+        Core.Variables.Keyboard.hookEvent(); //DONE
         Core.Variables.MainScene = new Scene('scene'); //DONE
-        Core.Variables.Map = new Map(); //TODO: CreateMapObject and realize reading of Map Object
-        Core.Variables.PlayerTank = new Tank(Core.Config.tileTankSrc, 0, 0, Core.Config.tileTankWidth, Core.Config.tileTankHeight, Core.Config.tankDirection, Core.Config.tankSpeed); //DONE
+        Core.Variables.MainScene.recalcSize(); //DONE
+        Core.Variables.Map = new Map(); //DONE
+        Core.Variables.Map.loadMask('../maps/test.map'); //TODO: realize param in object, which shows src to file with map
+        Core.Variables.Map.calculateOffsetMap(); //DONE
+        Core.Variables.Map.loadResources(); //DONE
+        Core.Variables.PlayerTank = new Tank(Core.Config.tileTankSrc, Core.Variables.Map.offsetXMap, Core.Variables.Map.offsetYMap, Core.Config.tileTankWidth, Core.Config.tileTankHeight, Core.Config.tankDirection, Core.Config.tankSpeed); //DONE
         Core.Variables.Game = new Game(); //DONE
         Core.Variables.GameTimer = new GameTimer(); //DONE
         Core.Variables.GameTimer.startGame(Core.Variables.Game); //DONE
